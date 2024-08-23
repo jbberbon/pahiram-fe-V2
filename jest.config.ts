adde4ea5c647320e1fa47450175c43bab2de8ac3
Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>
2024-08-23 21:00:40 +0800

    Fixed the jest issue and added HomePage test. All test passed. Will do the loginUser unit test.
    Signed-off-by: Mc Joseph Agbanlog <mcagbanlog@student.apc.edu.ph>

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     moduleNameMapper: {
         "^@/(.*)$": "./src/$1"
     },
-    setupFilesAfterEnv: ['./jest.setup.ts']
+    setupFilesAfterEnv: ['./jest.setup.ts'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
